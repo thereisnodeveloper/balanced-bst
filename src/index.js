d8ef4c070558249bb54de62a7dccfe79ac7ca5d6
amdev <56517440+thereisnodeveloper@users.noreply.github.com>
2024-10-11 13:27:33 -0400

    create Node class in module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const sampleArray1 = [7, 6, 5, 4, 3, 2, 1];
 const sampleArray2 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-//pre-process
-//sort array
-//remove duplicates
+// pre-process
+// sort array
+// remove duplicates
 
-//Node class
+// Node class
 class Node {
   constructor() {
     this.data;
@@ -14,10 +14,11 @@ class Node {
   }
 }
 
-//Tree class
+// Tree class
 
 class Tree {
   root = null;
+
   /**
    * @description
    * @param {Array} array
@@ -27,15 +28,15 @@ class Tree {
   buildTree(array) {
     return rootNode;
   }
+
   /**
      * @description function that takes an array of data and turns it into a
 balanced binary tree full of Node objects appropriately placed. The buildTree function should return the level-0 root node.
 
      * @param {Array} array
-     * @return {*} 
+     * @return {*}
      * @memberof Tree
      */
   preprocess(array) {}
 }
 
-//buildTree(array)
---
+++ src/node.js
@@ -0,0 +1,8 @@
+// Node class
+export default class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
